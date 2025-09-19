13a1f6b1326dacd38bb1a39f304a5d89c41c1570
TonsV2 <sebastianthegreatful@gmail.com>
2025-09-19 20:31:43 +0700

    chore: remove echo debugging commands

--- release.sh
+++ release.sh
@@ -4,6 +4,6 @@ VERSION=$(yq '.project.version' pyproject.toml)
 IMAGE_BASE=$(yq '.services.app.image' docker-compose.yml | sed 's/:latest$//')
 
 docker compose build
-echo docker tag "$IMAGE_BASE":latest "$IMAGE_BASE:$VERSION"
-echo docker push "$IMAGE_BASE:latest"
-echo docker push "$IMAGE_BASE:$VERSION"
+docker tag "$IMAGE_BASE":latest "$IMAGE_BASE:$VERSION"
+docker push "$IMAGE_BASE:latest"
+docker push "$IMAGE_BASE:$VERSION"
